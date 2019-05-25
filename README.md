783d9261f8f5aa5cbab880e5097a959a76bbf08a
Shilin Victor <chrono.monochrome@gmail.com>
2019-05-25 15:10:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## node_static ![node_static](https://img.shields.io/npm/v/node_static.svg)
+
+> 
+
+### Installation
+
+```bash
+$ npm install node_static
+```
+
+### Example
+
+```js
+const node_static = require('node_static');
+
+// your code here
+
+```
+
+### Contributing
+- Fork this Repo first
+- Clone your Repo
+- Install dependencies by `$ npm install`
+- Checkout a feature branch
+- Feel free to add your features
+- Make sure your features are fully tested
+- Publish your local branch, Open a pull request
+- Enjoy hacking <3
+
+### MIT
+
+This work is licensed under the [MIT license](./LICENSE).
+
+---
---
+++ app.js
@@ -0,0 +1,115 @@
+require('dotenv').config()
+
+var process = require('process');
+var path = require('path');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var url = require('url');
+var nunjucks = require('nunjucks');
+
+function getExternalFile(pathToFile) {
+	return path.join(path.dirname(process.execPath), pathToFile);
+}
+
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+var http = require('http');
+var key = require('./keys/key.js');
+var key2 = require('./keys/key2.js');
+
+function xor(arr1, arr2) {
+	var res = new Int32Array(arr1.length);
+	for (var i = 0; i < arr1.length; i++) {
+		res[i] = (arr1[i] ^ arr2[i]);
+	}
+
+	return res;
+}
+
+
+function bufferToString(arr){
+	var str = "";
+	for (i in arr){
+		str+=String.fromCharCode(arr[i]);
+	}
+	return str;
+}
+
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app
+});
+
+function decode(body, key) {
+	var buff = new Buffer(body.toString(), 'base64')
+	//console.dir(buff);
+	//console.dir(key);
+	var r = xor(buff, key);
+
+	return bufferToString(r);
+}
+
+function do_GET(options, callback, cb_args) {
+	var req_ = http.get(options, function(res_) {
+
+		// Buffer the body entirely for processing as a whole.
+		var bodyChunks = [];
+		res_.on('data', function(chunk) {
+			// You can process streamed parts here...
+			bodyChunks.push(chunk);
+		}).on('end', function() {
+			var body = Buffer.concat(bodyChunks);
+
+			callback(body);
+		})
+	});
+
+	req_.on('error', function(e) {
+		console.log('ERROR: ' + e.message);
+	});
+}
+
+app.get('/animes/:anime_id/video_online/:episode', function(req, res) {
+	var anime_id = req.params.anime_id;
+	var episode = req.params.episode;
+
+	var videos_options = {
+		host: process.env.HOST,
+		port: process.env.PORT,
+		path: '/api/' + anime_id + '/' + episode
+	};
+
+	var anime_options = {
+		host: process.env.HOST,
+		port: process.env.PORT,
+		path: '/' + anime_id
+	};
+
+	do_GET(videos_options, function(anime_videos_res, callback, cb_args) {
+		var anime_videos = JSON.parse(decode(anime_videos_res, key2));
+		for (kind of ["fandub", "raw", "subtitles"]) {
+			
+			for (var i = 0; i < anime_videos[kind].length; i++) {
+				anime_videos[kind][i]["url"] = decode(anime_videos[kind][i].url, key);
+				anime_videos[kind][i]["video_hosting"] = url.parse(anime_videos[kind][i].url).hostname;
+			}
+		}
+		anime_videos["active_video"].url = decode(anime_videos["active_video"].url, key);
+
+		do_GET(anime_options, function(anime_info_res, callback, cb_args) {
+			var anime_info = JSON.parse(decode(anime_info_res, key2));
+			res.render('video_template.html', {"anime_id": anime_id, "episode": episode, "anime_videos": anime_videos, "anime_info": anime_info, "static": ""});
+		});
+	});
+
+});
+
+app.use(express.static(getExternalFile("public")))
+
+app.listen(3000, "0.0.0.0", function () {
+	console.log('Shikimori app listening on port 3000!');
+	console.log('Serving files from ' + getExternalFile("public"));
+});
